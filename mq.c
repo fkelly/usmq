d247fd31a0e06682216c24c84febde4fedcfd537
Francis Kelly <francis.kelly@gmail.com>
2011-06-13 11:01:18 -0700

    license lgpl

--- mq.c
+++ mq.c
@@ -1,19 +1,20 @@
 /*
 Ultra Simple Message Queue 
+
 Copyright (C) 2011  Francis Kelly (francis.kelly@gmail.com)
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
+
+http://www.gnu.org/licenses/lgpl-3.0.txt
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /* Messages have the following form:
